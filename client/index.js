3d80780cbdb38d898c73b3bbce97e8826e5555b3
mfiloramo <mlfiloramo@gmail.com>
2022-03-13 22:30:03 -0400

    refactored webpack.confis.js

--- client/index.js
+++ client/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
-import App from './App';
+import App from './App.jsx';
 import store from './store';
-import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 render(
   <BrowserRouter>
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, '/dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        // THIS CAN BE EXPRESSED AS A SIMPLE ARRAY OF STRINGS -- NOTE THE MODULAR SYNTAX
+      //   use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      // }
+        use: [{
+          loader: 'style-loader', // INJECTS CSS TO PAGE
+        }, {
+          loader: 'css-loader', // TRANSLATES CSS INTO CommonJS MODULES
+        }, {
+          loader: 'postcss-loader', // RUN POST CSS ACTIONS
+        }, {
+          loader: 'sass-loader', // COMPILE SASS TO CSS
+        }]
+      },
+    ]
+  },
+  mode: 'development',
+  devServer: {
+    host: 'localhost',
+    port: 8083,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    hot: true,
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3002',
+        secure: false,
+      },
+      '/assets/**': {
+        target: 'http://localhost:3002',
+        secure: false,
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    })
+  ]
+
+};
