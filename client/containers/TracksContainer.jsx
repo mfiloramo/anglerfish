b3404dee26ba4ce8f3fe85349d6f233551b511d6
mfiloramo <mlfiloramo@gmail.com>
2022-03-24 11:25:41 -0400

    frontend add/deleteSong functions operational

--- client/containers/TracksContainer.jsx
+++ client/containers/TracksContainer.jsx
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
 // TODO: IMPORT SONG CREATOR THAT WILL QUERY FOR SONGS
 
-class TracksContainer extends Component {
-  render() {
-    const { addTrack, deleteTrack, trackList, totalTracks } = this.props;
+let key = 0;
 
+class TracksContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      songs: []
+    }
+
+    this.addSong = this.addSong.bind(this);
+    this.deleteSong = this.deleteSong.bind(this);
+
+  }
+  addSong = () => {
+    const newState = [...this.state.songs];
+    newState.push(
+      <tr key={key++}>this is an added song!</tr>
+    );
+    this.setState({ songs: newState });
+    console.log(key)
+  };
+
+  deleteSong = () => {
+    const newState = [...this.state.songs];
+    newState.pop();
+    this.setState({ songs: newState });
+  };
+
+  render() {
     return (
       <div className={'innerBox'}>
-        <h4 id={"test"}>This is the TracksContainer component</h4>
+        <button
+          type={'button'}
+          onClick={this.addSong}>
+          Add song
+        </button>
+        <button
+          type={'button'}
+          onClick={this.deleteSong}>
+          Delete song
+        </button>
+        <h4 id={"test"}>This is the TracksContainer component, and these are the songs: { this.state.songs }</h4>
       </div>
     );
   }
