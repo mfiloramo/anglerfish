7609af4b5b81e5e4bc067dac3ed47615ced401db
mfiloramo <mlfiloramo@gmail.com>
2022-03-24 18:23:35 -0400

    frontend addSong and deleteSong operational (buttons/inputs)

--- client/containers/TracksContainer.jsx
+++ client/containers/TracksContainer.jsx
@@ -1,50 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 // TODO: IMPORT SONG CREATOR THAT WILL QUERY FOR SONGS
 
+// USE AND INCREMENT A NUMBER TO ASSIGN UNIQUE KEY IDS TO COMPONENTS
 let key = 0;
 
 class TracksContainer extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      songs: []
-    }
+    this.state = { songs: [] }
 
     this.addSong = this.addSong.bind(this);
     this.deleteSong = this.deleteSong.bind(this);
+  };
 
-  }
-  addSong = () => {
-    const newState = [...this.state.songs];
-    newState.push(
-      <tr key={key++}>this is an added song!</tr>
+  addSong = e => {
+    if (e.keyCode === 13) {
+      const newState = this.state.songs;
+      newState.push(
+        <div key={key++}>This is a new song. It is by {e.target.value}</div>,
+        // <br />
     );
-    this.setState({ songs: newState });
-    console.log(key)
+      this.setState({ songs: newState });
+      console.log('Song added!');
+    }
   };
 
   deleteSong = () => {
-    const newState = [...this.state.songs];
-    newState.pop();
-    this.setState({ songs: newState });
+      const newState = this.state.songs;
+      newState.pop();
+      this.setState({ songs: newState });
+      console.log('Song deleted!');
   };
 
   render() {
     return (
       <div className={'innerBox'}>
-        <button
-          type={'button'}
-          onClick={this.addSong}>
-          Add song
-        </button>
-        <button
-          type={'button'}
-          onClick={this.deleteSong}>
-          Delete song
-        </button>
-        <h4 id={"test"}>This is the TracksContainer component, and these are the songs: { this.state.songs }</h4>
+
+        <button type={'button'} onClick={this.deleteSong}>Delete Song</button>
+          <br />
+        <label htmlFor={'artist'}>Artist: </label>
+        <input id={'a'} type={'text'} name={'artist'} onKeyDown={this.addSong} />
+          <br />
+        <label htmlFor={'song'}>Song: </label>
+        <input id={'a'} type={'text'} name={'song'} onKeyDown={this.addSong} />
+
+        <div>
+          {this.state.songs}
+        </div>
+
       </div>
+
+      // {/*// <table className={'innerBox'}>*/}
+      // {/*//   <tbody>*/}
+      // {/*//   <tr>*/}
+      // {/*//     <td>*/}
+      // {/*//       <button type={'button'} onClick={this.deleteSong}>Delete Song</button>*/}
+      // {/*//         <br />*/}
+      // {/*//       <label htmlFor={'artist'}>Artist: </label>*/}
+      // {/*//       <input id={'a'} type={'text'} name={'artist'} onKeyDown={this.addSong} />*/}
+      // {/*//         <br />*/}
+      // {/*//       <label htmlFor={'song'}>Song: </label>*/}
+      // {/*//       <input id={'a'} type={'text'} name={'song'} onKeyDown={this.addSong} />*/}
+      // {/*//     </td>*/}
+      // {/*//     <td>*/}
+      // {/*//       {this.state.songs}*/}
+      // {/*//     </td>*/}
+      // {/*//*/}
+      // {/*//   </tr>*/}
+      // {/*//   </tbody>*/}
+      // {/*// </table>*/}
     );
   }
 }
--- server/server.js
+++ server/server.js
@@ -5,14 +5,13 @@ const app = express();
 const PORT = 3009;
 
 /** REQUIRE ROUTERS */
-// CONSTRUCT ROUTERS HERE
-
+// WIRE ROUTERS HERE
 
 /** HANDLE PARSING REQUEST BODY */
 app.use(express.json());
 
 /** HANDLE REQUESTS FOR STATIC FILES */
-app.use('/assets', express.static(path.resolve(__dirname, 'client/assets/')));
+app.use(express.static('./client/assets/', path.resolve(__dirname, './client/assets/')));
 
 /** DEFINE ROUTE HANDLERS */
 
@@ -23,14 +22,24 @@ app.get('/', (request, response) => {
 })
 
 /** CATCH-ALL ROUTE HANDLER FOR ANY REQUESTS TO AN UNKNOWN ROUTE */
-
+app.use('*', (request, response) => {
+  response.status(404).send('Error: Page not found');
+});
 
 /** CONFIGURE EXPRESS GLOBAL ERROR HANDLER */
-
+app.use((error, request, response, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign(defaultErr, { error: error });
+  response.status(errorObj.status).json(errorObj.message.err);
+});
 
 /** START SERVER */
 app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+  console.log(`Server connected -- listening on port: ${PORT}`);
 });
 
 module.exports = app;
